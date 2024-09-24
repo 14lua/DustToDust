cfc69162d0966836dbcc22ec107c220393f61f3c
Luan Xhigoli <xhigoliluan022@gmail.com>
2024-09-24 22:19:08 +0200

    Starting Menu

---
+++ src/Globals.cs
@@ -0,0 +1,6 @@
+﻿namespace DustToDust;
+
+public static class Globals
+{
+    public static string UsrName { get; set; } = "Yonah";
+}
---
+++ src/Program.cs
@@ -0,0 +1,38 @@
+﻿using Spectre.Console;
+using Figgle;
+using SharpConfig;
+using CommandLine;
+
+namespace DustToDust;
+
+/*
+ * SpectreConsole for pretty Console
+ * Figgle for ascii art, mainly the title
+ * SharpConfig to make configuration easier
+ * CommandLineParser to handle input better
+ */
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        WelcomeMessage(args);
+    }
+
+    private static void WelcomeMessage(string[] args)
+    {
+        var ascii = FiggleFonts.Computer.Render("Dust To Dust");
+        var title = new Markup($"\n\n[red]{ascii}[/]\n\n").Centered();
+        Console.Clear();
+        AnsiConsole.Write(title);
+
+        var choice = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("What do you want to do?")
+                .PageSize(10)
+                .HighlightStyle(Style.Plain)
+                .AddChoices(new[] { "Log In", "About", "Exit" }));
+
+        Console.WriteLine($"You chose {choice}");
+    }
+}
